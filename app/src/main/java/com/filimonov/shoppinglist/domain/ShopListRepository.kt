f30a02dccfee74aefcee165c442ebe190d88b1a5
Andrey Filimonov <andrfil05@mail.ru>
2025-07-06 18:05:02 +0600

    Create usecases

---
+++ app/src/main/java/com/filimonov/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,14 @@
+package com.filimonov.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun getShopItem(itemId: Int): ShopItem
+
+    fun getShopList(): List<ShopItem>
+}
